c82b049585d0f7a8e89aa9ef751e55514a06c0b0
anreetka <anreetka@sheridancollege.ca>
2024-11-06 12:28:47 -0500

    added LargeMemoryOwner implementation

---
+++ csharp/src/Apache.Arrow/Memory/LargeMemoryOwner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Apache.Arrow.Memory
+{
+    internal class LargeMemoryOwner<T>:IDisposable
+    {
+        private readonly LargeMemory<T> _memory;
+
+        public LargeMemoryOwner(long size)
+        {
+            _memory = new LargeMemory<T>(size);
+        }
+
+        public LargeMemory<T> Memory {  get { return _memory; } }
+
+        public void Dispose()
+        {
+            _memory.Dispose();
+        }
+    }
+}
