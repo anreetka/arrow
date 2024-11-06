ac9bbc98e3be9236be1c68204b463978a25efa71
anreetka <anreetka@sheridancollege.ca>
2024-11-06 12:18:13 -0500

    replaced implementation for LargeMemory and LargeSpan

---
+++ csharp/src/Apache.Arrow/Memory/LargeMemory.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Buffers;
+using System.Runtime.CompilerServices;
+
+namespace Apache.Arrow.Memory
+{
+    internal class LargeMemory<T>: IDisposable
+    {
+        private nint _ptr;
+        private long _size;
+
+        public LargeMemory(long size)
+        {
+            if(_size < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(size), "Length cannot be negative.");
+            }
+
+            _ptr = Marshal.AllocHGlobal((IntPtr)size);
+            _size = size;
+        }
+
+        public T this[long index]
+        {
+            get
+            {
+                if(index < 0 || index >= _size)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                return Marshal.PtrToStructure<T>(IntPtr.Add(_ptr, (int)(index * Marshal.SizeOf<T>())));
+            }
+
+            set
+            {
+                if (index < 0 || index >= _size)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                IntPtr targetAddr = IntPtr.Add(_ptr, (int)(index * Marshal.SizeOf<T>()));
+                Marshal.StructureToPtr<T>(value, targetAddr, false);
+                
+            }
+        }
+
+        public LargeMemory<T> Slice(long start, long end)
+        {
+            if (start < 0 || start + end > _size)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            IntPtr slicePtr = IntPtr.Add(_ptr, (int)(start * Marshal.SizeOf<T>()));
+            return new LargeMemory<T>(end) { _ptr = slicePtr };
+        }
+
+        public long Size { get { return _size; } }
+
+        public void Dispose()
+        {
+            if (_ptr != IntPtr.Zero)
+            {
+                Marshal.FreeHGlobal(_ptr);
+                _ptr = IntPtr.Zero;
+            }
+        }
+
+
+
+    }
+}
---
+++ csharp/src/Apache.Arrow/Memory/LargeSpan.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Apache.Arrow.Memory
+{
+    internal class LargeSpan<T>:IDisposable
+    {
+        private readonly LargeMemory<T> _memory;
+        private long _offset;
+        private long _length;
+
+        public LargeSpan(LargeMemory<T> memory, long offset, long length)
+        {
+            if(memory == null) throw new ArgumentNullException(nameof(memory), "Memory cannot be null.");
+            if (offset < 0) throw new ArgumentOutOfRangeException(nameof(offset), "Start index cannot be negative.");
+            if(length < 0) throw new ArgumentOutOfRangeException(nameof(length), "Length cannot be negative.");
+
+            if( offset + length > _memory.Size)
+            {
+                throw new ArgumentOutOfRangeException(nameof(length), "The requested span exceeds the available memory size.");
+            }
+
+            _memory = memory;
+            _offset = offset;
+            _length = length;
+        }
+
+        public long Length { get { return _length; } }
+
+        public T this[long index]
+        {
+            get
+            {
+                if (index < 0 || index >= _length)
+                {
+                    throw new IndexOutOfRangeException("Index is out of range within the span.");
+                }
+
+                return _memory[_offset + index];
+            }
+
+            set
+            {
+                if (index < 0 || index >= _length)
+                {
+                    throw new IndexOutOfRangeException("Index is out of range within the span.");
+                }
+
+                _memory[_offset + index] = value;
+            }
+        }
+
+        public void Dispose()
+        {
+         
+        }
+    }
+}
