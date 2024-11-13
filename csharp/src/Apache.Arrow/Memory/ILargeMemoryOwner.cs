80f8672603a507100a4330b47088d7601c318d57
anreetka <anreetka@sheridancollege.ca>
2024-11-13 09:13:06 -0500

    implemented ILargeMemoryowner and LargeMemoryManager

---
+++ csharp/src/Apache.Arrow/Memory/ILargeMemoryOwner.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Apache.Arrow.Memory
+{
+    public interface ILargeMemoryOwner<T>:IDisposable
+    {
+        LargeMemory<T> LargeMemory { get; }
+    }
+}
---
+++ csharp/src/Apache.Arrow/Memory/LargeMemoryManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+
+namespace Apache.Arrow.Memory
+{
+    public abstract class LargeMemoryManager<T>:ILargeMemoryOwner<T>, IPinnable
+    {
+        public virtual LargeMemory<T> LargeMemory => new LargeMemory<T>(this, GetSpan().Length);
+
+        public abstract Span<T> GetSpan();
+
+        public abstract MemoryHandle Pin(long elementIndex = 0);
+
+        public abstract void Unpin();
+
+        protected LargeMemory<T> CreateMemory(long length) => new LargeMemory<T>(this, length);
+
+        protected LargeMemory<T> CreateMemory(long start, long length) => new LargeMemory<T>(this, start, length);
+
+        protected internal virtual bool TryGetArray(out ArraySegment<T> segment)
+        {
+            segment = default;
+            return false;
+        }
+
+        void IDisposable.Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected abstract void Dispose(bool disposing);
+
+    }
+}
